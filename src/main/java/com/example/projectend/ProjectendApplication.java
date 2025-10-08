5d160b95558a555aef11f817f5b5f3decc72d666
uyenntp207 <phuonguyennguyen160707@gmail.com>
2025-10-08 10:35:14 +0700

    gioithieu

--- src/main/java/com/example/projectend/ProjectendApplication.java
+++ src/main/java/com/example/projectend/ProjectendApplication.java
@@ -2,16 +2,26 @@ package com.example.projectend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * MAIN APPLICATION CLASS
  * Người 1 - Database Design & Backend Core
  */
 @SpringBootApplication
-public class ProjectendApplication {
+public class ProjectendApplication implements CommandLineRunner {
+
+    @Value("${server.port:8080}")
+    private String port;
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectendApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println("Application is running at: http://localhost:" + port + "/");
     }
 
 }
